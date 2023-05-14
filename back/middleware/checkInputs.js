6532f7058ebb75d0f99f0d06fd388bcfaec2331b
pharouqy <pharouqy@gmail.com>
2023-05-14 08:41:21 +0100

    CheckInputs Done !!!

---
+++ back/middleware/checkInputs.js
@@ -0,0 +1,27 @@
+module.exports = (req, res, next) => {
+  const regexAlpha = /^[a-zA-Z, àâäéèêëîïôöùûüÿçÂÊÎÔÛÄËÏÖÜÀÆÇÉÈŒÙ&]+$/;
+  const regexNumber = /^[0-9]+$/;
+  // si la requête est une requête POST ou PUT et qu'il y a un fichier
+  const book = JSON.parse(req.body.book);
+  const { title, author, year, genre } = book; // ES6 destructuration
+  checkInput(req, res, next, title, author, year, genre);
+  function checkInput(res, next, title, author, year, genre) {
+    if (
+      title.length >= 3 &&
+      author.length >= 3 &&
+      year.length == 4 &&
+      genre.length >= 3 &&
+      regexAlpha.test(title) &&
+      regexAlpha.test(author) &&
+      regexNumber.test(year) &&
+      regexAlpha.test(genre)
+    ) {
+      next();
+    } else {
+      res.status(400).json({
+        message:
+          "vous devez remplir tous les champs avec au moins 3 caractéres sans utiliser de symbole spéciale !!!",
+      });
+    }
+  }
+};
--- back/routes/book.js
+++ back/routes/book.js
@@ -4,13 +4,15 @@ const router = express.Router();
 const auth = require("../middleware/auth");
 const multer = require("../middleware/multer-config");
 
+const inputsCtrlr = require("../middleware/checkInputs");
+
 const booksContrlr = require("../controllers/book");
 
-router.post("/", auth, multer, booksContrlr.createBook);
+router.post("/", auth, multer, inputsCtrlr, booksContrlr.createBook);
 router.get("/", booksContrlr.getAllBooks);
 router.get("/bestrating", booksContrlr.bestratingBook);
 router.get("/:id", booksContrlr.getOneBook);
-router.put("/:id", auth, multer, booksContrlr.updateBook);
+router.put("/:id", auth, multer, inputsCtrlr, booksContrlr.updateBook);
 router.delete("/:id", auth, booksContrlr.deleteBook);
 router.post("/:id/rating", auth, booksContrlr.ratingBook);
 
