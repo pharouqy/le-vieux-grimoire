27add996e2137a13d38fcb727da1153e72346069
pharouqy <pharouqy@gmail.com>
2023-05-14 08:22:24 +0100

    Limiter added !!!

---
+++ back/middleware/limiter.js
@@ -0,0 +1,8 @@
+const rateLimit = require("express-rate-limit"); // Limite le nombre de requêtes et éviter les attaques (Brute Force & DDOS)
+
+module.exports = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10, // Limit each IP to 10 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+});
--- back/routes/user.js
+++ back/routes/user.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
+const limiter = require("express-rate-limit");
+
 const userContrlr = require("../controllers/user");
 
 router.post("/signup", userContrlr.signup);
-router.post("/login", userContrlr.signin);
+router.post("/login", limiter, userContrlr.signin);
 
 module.exports = router;
